3a23e376f51ec328799fedf8e32d6a9ba00b215f
Alex Khmurovich <alexhmurovich@gmail.com>
2024-08-14 19:37:00 -0500

    User input and changes to home screen

--- geo-quiz-web/src/pages/Capitals.js
+++ geo-quiz-web/src/pages/Capitals.js
@@ -3,14 +3,30 @@ import CountryData from "../CountryData.json";
 
 export default function Capitals() {
    const [index, setIndex] = useState(0);
+   const [userInput, setUserInput] = useState("");
 
    function getRandomCountry() {
       setIndex(Math.floor(Math.random() * CountryData.length));
    }
 
+   function checkUserAnswer() {
+      if (userInput.trim().toUpperCase() == CountryData[index].capital) {
+         console.log("Right");
+      }
+   }
+
    return (
       <>
+         <h1 className="text-5xl">Capitals</h1>
          <h1>{CountryData[index].name}</h1>
+         <input
+            placeholder="Enter the capital"
+            value={userInput}
+            onChange={(e) => setUserInput(e.target.value)}
+         />
+         <button className="block" onClick={checkUserAnswer}>
+            Submit
+         </button>
          <button onClick={getRandomCountry}>Get Random Country</button>
       </>
    );
--- geo-quiz-web/src/pages/Card.js
+++ geo-quiz-web/src/pages/Card.js
@@ -1,12 +1,17 @@
 function Card(props) {
-    return (
-      <div className="border-sky-600 border-2 p-2 m-2">
-        <img src={props.img}/>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-        <a href={props.link}>Play</a>
+   return (
+      <div className="border-sky-600 border-2 p-2 m-2 rounded-lg flex flex-col">
+         <img src={props.img} />
+         <h1 className="font-bold text-lg">{props.title}</h1>
+         <p className="text-gray-700">{props.description}</p>
+         <a
+            className="bg-sky-600 text-white p-2 rounded-lg w-fit"
+            href={props.link}
+         >
+            Play
+         </a>
       </div>
-    );
-  }
-  
+   );
+}
+
 export default Card;
