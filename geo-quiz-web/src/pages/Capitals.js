67176606f2dbc6360cacde54419008ed54a467d5
Alex Khmurovich <alexhmurovich@gmail.com>
2024-08-14 19:47:34 -0500

    ui changes, added setTimeout, auto moves to next quesiton

--- geo-quiz-web/src/pages/Capitals.js
+++ geo-quiz-web/src/pages/Capitals.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 import CountryData from "../CountryData.json";
 
 export default function Capitals() {
-   const [index, setIndex] = useState(0);
+   const [index, setIndex] = useState(
+      Math.floor(Math.random() * CountryData.length)
+   );
    const [userInput, setUserInput] = useState("");
+   const [showResponse, setShowResponse] = useState(false);
 
    function getRandomCountry() {
       setIndex(Math.floor(Math.random() * CountryData.length));
@@ -12,22 +15,29 @@ export default function Capitals() {
    function checkUserAnswer() {
       if (userInput.trim().toUpperCase() == CountryData[index].capital) {
          console.log("Right");
+         setShowResponse(true);
       }
+
+      setTimeout(() => {
+         setShowResponse(false);
+         setUserInput("");
+         getRandomCountry();
+      }, 1000);
    }
 
    return (
       <>
          <h1 className="text-5xl">Capitals</h1>
-         <h1>{CountryData[index].name}</h1>
+         <h1>{CountryData[index].name[0]}</h1>
          <input
             placeholder="Enter the capital"
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
          />
+         <h1 className={showResponse ? "block" : "hidden"}>Correct</h1>
          <button className="block" onClick={checkUserAnswer}>
             Submit
          </button>
-         <button onClick={getRandomCountry}>Get Random Country</button>
       </>
    );
 }
--- geo-quiz-web/src/pages/Home.js
+++ geo-quiz-web/src/pages/Home.js
@@ -1,11 +1,23 @@
-import Card from './Card.js'
+import Card from "./Card.js";
 
-export default function Home(){
-    return(
-        <div className="App">
-            <h1 className="font-bold">Welcome to GeoQuiz</h1>
-            <Card title="Flags" description="Guess the flags of the world's countries." img="asdf" link="/flags"></Card>
-            <Card title="Capitals" description="Guess the capitals of the world's countries." img="asdf" link="/capitals"></Card>
-        </div>
-        )
+export default function Home() {
+   return (
+      <div className="App">
+         <h1 className="font-bold">Welcome to GeoQuiz</h1>
+         <div className="flex">
+            <Card
+               title="Flags"
+               description="Guess the flags of the world's countries."
+               img="asdf"
+               link="/flags"
+            ></Card>
+            <Card
+               title="Capitals"
+               description="Guess the capitals of the world's countries."
+               img="asdf"
+               link="/capitals"
+            ></Card>
+         </div>
+      </div>
+   );
 }
