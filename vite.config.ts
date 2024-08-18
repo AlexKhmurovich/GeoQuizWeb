b458bb61016200bc92019b7617368866d2b08e22
Alex Khmurovich <alexhmurovich@gmail.com>
2024-08-18 18:23:28 -0500

    added a hash router

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base:"/GeoQuizWeb/",
+  base:"/GeoQuizWeb",
   plugins: [react()],
   resolve: {
     alias: {
